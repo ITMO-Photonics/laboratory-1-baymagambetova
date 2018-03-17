4a7eb9a454217ca52f9e8614c3aef2e6e7213eeb
user1 <rose18.2013@gmail.com>
2018-03-17 15:00:52 +0300

    created picture

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
+import math
+pi = 3.14
+def calculate(R, L):
+	volume = pi * R * R * L
 
+	return ('Volume: ' + str(volume))
+
+R = int(input('please Enter the radius of a Cylinder: '))
+L = int(input('please Enter the length of a Cylinder: '))
+
+print(calculate(R,L))
