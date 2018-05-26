fff428900c6051e4e6d37be25ea87da21881ef26
user1 <ninaashotovna@mail.ru>
2018-05-26 14:15:52 +0300

    message

--- task3.py
+++ task3.py
@@ -1,1 +1,13 @@
+import numpy as np
+import scipy.linalg as lng
 
+N=10
+A = np.zeros((N,N))
+i,j = np.indices(A.shape)
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+3]=1
+#A[i<j]=2
+B = np.arange(N)
+F = np.linalg.solve (A,B)
+print(F)
